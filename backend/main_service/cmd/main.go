cfa34e06fd5a57f2759755500c90e639b07d9c6c
Lalit-h2 <lhcloud8@gmail.com>
2026-03-01 20:48:18 +0530

    add cors
    added chi/cors package to manage cors option

--- backend/main_service/cmd/main.go
+++ backend/main_service/cmd/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Lalit-h2/DF-Detector/backend/main_service/pkg/routers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	_ "github.com/lib/pq"
 )
 
@@ -20,7 +21,16 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins: []string{"*"},
+			AllowedMethods: []string{"GET", "POST", "PUT"},
+			// AllowedHeaders: []string{"Accept","Authorization","Content-Tpe"},
+			AllowedHeaders: []string{"*"},
+		},
+	))
 	routers.RegisterRoutes(r)
 	defer grpc_service.CloseConnection()
+
 	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
--- backend/main_service/go.mod
+++ backend/main_service/go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/lib/pq v1.11.2 // indirect
 	golang.org/x/net v0.49.0 // indirect
